1c76e5b3d3049749cc5ff5c452c2f9313b1a5fd4
Nikita Chemeris <nikitachemeris@Nikitas-MacBook-Pro.local>
2023-07-14 22:45:02 +0200

    Changed Task_2, Lection_1

--- Lection_1/Task_2/add_operation.cs
+++ Lection_1/Task_2/add_operation.cs
@@ -3,6 +3,4 @@ Console.WriteLine("Enter number: ");
 
 int inputNumber = Convert.ToInt32(Console.ReadLine());
 
-int result = inputNumber + 10;
-
-Console.WriteLine($"Your number + 10 = { result }");
+Console.WriteLine($"Your number + 10 = { inputNumber + 10 }");
